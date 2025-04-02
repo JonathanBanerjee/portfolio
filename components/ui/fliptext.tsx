8a5f7aa905a55657a35120329a58466a6f9e7aca
jonathanbanerjee <jonathan@youd.net>
2025-04-02 16:52:01 +0100

    adding flipwords colours

--- components/ui/fliptext.tsx
+++ components/ui/fliptext.tsx
@@ -8,7 +8,11 @@ export function FlipText() {
     <div className="h-[20rem] flex flex-col justify-center items-center px-4 text-center">
       <div className="text-4xl font-normal text-neutral-600 dark:text-neutral-400">
         Hi, I'm Jonathan.
-        <br /> A <FlipWords words={words} />
+        <br /> A{" "}
+        <FlipWords
+          words={words}
+          className="text-blue-600 dark:text-orange-400"
+        />
         developer crafting exceptional digital experiences.
       </div>
     </div>
