afb65fba0ecd25d6294a74cd2935efb645266b9b
jonathanbanerjee <jonathan@youd.net>
2025-04-23 15:08:16 +0100

    returning home

--- components/ui/projects.tsx
+++ components/ui/projects.tsx
@@ -13,7 +13,7 @@ export const projects = [
   {
     title: "Portfolio",
     description: "This Website. showcasing my latest and greatest projects.",
-    link: "jonathanbanerjee.vercel.app",
+    link: "/",
     githubLink: "https://github.com/JonathanBanerjee/portfolio",
     icon: <Github className="w-4 h-4" />,
     badges: [
